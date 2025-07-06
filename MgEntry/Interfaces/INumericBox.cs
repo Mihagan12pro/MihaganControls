2594e74f73a496137b923b25e171066c1cdd8908
Mihagan12pro <mikhail.kargin.05@mail.ru>
2025-07-06 14:35:42 +0300

    feat: adding the INumericBox interface.

---
+++ MgEntry/Interfaces/INumericBox.cs
@@ -0,0 +1,14 @@
+﻿
+namespace MihaganControls.MgEntry.Interfaces
+{
+    internal interface INumericBox <T>
+    {
+        T Maximum { get; set; }
+
+        T Minimum { get; set; }
+
+        bool StrictlyMinimum { get; set; }
+
+        bool StrictlyMaximum { get; set; }
+    }
+}
